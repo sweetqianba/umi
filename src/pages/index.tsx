ba954184252cc6ba00530b81aeb586ccfabdb284
liuyuanhao <liuyuanhao@qianxin.com>
2020-10-12 17:08:09 +0800

    gittosvn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default () => {
   return (
     <div>
       <h1>success!!!!!</h1>
-      <h1 className={styles.title}>Page index 1112333 </h1>
+      <h1 className={styles.title}>Page index </h1>
     </div>
   );
 };
