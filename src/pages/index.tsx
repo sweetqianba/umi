b1566886fe80d933f32149020aa210e5c59b341d
liuyuanhao <liuyuanhao@qianxin.com>
2020-10-12 16:56:26 +0800

    git to svn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.less';
 export default () => {
   return (
     <div>
-      <h1>success!!!</h1>
+      <h1>success!!!!</h1>
       <h1 className={styles.title}>Page index 1112333 </h1>
     </div>
   );
