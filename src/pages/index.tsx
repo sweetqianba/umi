93caf74031f0632174b8bfb2de8b142e0c2d4edd
liuyuanhao <liuyuanhao@qianxin.com>
2020-10-12 15:58:13 +0800

    undefined

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.less';
 export default () => {
   return (
     <div>
-      <h1>pppppp</h1>
+      <h1>00000</h1>
       <h1 className={styles.title}>Page index 1112333 </h1>
     </div>
   );
